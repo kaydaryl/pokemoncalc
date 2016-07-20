ca988a7692c86905c818440be64e2a8723807e6f
Neil Primmer <nprimmer@pixsystem.com>
2016-07-20 13:39:42 -0700

    Comments, if you care to implement

--- calc.py
+++ calc.py
@@ -1,7 +1,12 @@
 import sys
 import os
+# See note in pokedex.py regarding this.
 from pokedex import pokedex
 
+# Anything that's programmatically determined later in the script, or that's 
+# input by the user, doesn't need to be declared here in advance with a 
+# non-useful value, unless you're using it to catch exceptions. If you 
+# are doing so, use the false boolean, rather than integer values.
 numPoke = 0
 numEvolv = 0
 numCandy = -1
@@ -16,17 +21,36 @@ numEvolvcount = 0
 
 print "Daryl's PokemonGo Lucky Egg Evolution calculator"
 
+# I'd suggest avoiding while loops when you don't actually need to loop through 
+# anything. Your script is currently written to run once and query its 
+# parameters manually. 
+# I'd suggest replacing this whole block with:
+# try:
+#     pokeName = str(raw_input("Enter the name of your Pokemon: ").lower())
+#     numEvolv = pokedex[pokeName]
+# except:
+#     print 'Invalid or unclassified Pokemon name entered'
+#     exit(1)
+#
+# If you later architect the script to run on a loop overall, I'd suggest 
+# packing the whole script into a loop that iterates on command line arguments
 while numEvolv == 0:
         pokeName = str(raw_input("Enter the name of your Pokemon: ").lower())
 	for key in pokedex:
                 if str(key) == pokeName:
                         numEvolv = pokedex[pokeName]
+
+# See above note about loops. In this case, you're trying to use a loop to 
+# replace a conditional, though the conditional doesn't need to be there 
+# anyway. What you really want is a try-except clause to take corrective
+# action if the input doesn't match what is expected.
 while numPoke <= 0:
 	numPoke = int(input("How many " + pokeName + " do you have? "))
 while numCandy < 0:
 	numCandy = int(input("How many " + pokeName + " candy do you have? "))
 
-
+# I don't see anything that really needs improvement past here, except for the
+# best practices mentioned above regarding variable declaration. 
 if (numCandy % numEvolv) != 0:
 	while ((numCandy + numCandyNew) % numEvolv) != 0:
 		numPokeNew += 1
