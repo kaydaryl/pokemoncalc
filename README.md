ce0d2ed2c816f52bd155b91976e2285c4a0d8462
server <kaydaryl@gmail.com>
2016-07-21 10:13:24 -0700

    v0.1

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
-##### Readme for Daryl's PokemonGO predictive lucky egg calculator
+#### Readme for Daryl's PokemonGO predictive lucky egg calculator
 
+#### Requires Python 2.x. Please install from python.org first!
+
+To use, execute "python calc.py" or "python calc.py -h for arguments"
+If you do not pass all arguments, you will be prompted to enter them.
+
+ex: calc.py -p charmander -n 2 -c 10
+
+NOTE:
 For Nidoran♀ and Nidoran♂ type Nidroanm or Nidoranf.
 
 Thank you for using!
-
 If you'd like to donate:
 
     https://paypal.me/kaydaryl/
--- calc.py
+++ calc.py
@@ -1,84 +1,85 @@
 import sys
 import os
 import json
-
-numPoke = False
-numEvolv = False
-numCandy = False
-numPokeNew = False
-numCandyNew = False
-numEvolvcount = False
-with open('pokedex.json', 'r') as f:
-	pokedex = json.load(f)
-
-#command to use an .ini file for settings
-#settings=ReadSettings(os.path.dirname(sys.argv[0]), "autoProcess.ini", logger=log)
-#then just use settings.variable
-
+import getopt
+import argparse
 
 print "Daryl's PokemonGo Lucky Egg Evolution calculator"
 
-# I'd suggest avoiding while loops when you don't actually need to loop through 
-# anything. Your script is currently written to run once and query its 
-# parameters manually. 
-# I'd suggest replacing this whole block with:
-# try:
-#     pokeName = str(raw_input("Enter the name of your Pokemon: ").lower())
-#     numEvolv = pokedex[pokeName]
-# except:
-#     print 'Invalid or unclassified Pokemon name entered'
-#     exit(1)
-#
-# If you later architect the script to run on a loop overall, I'd suggest 
-# packing the whole script into a loop that iterates on command line arguments
 
-try:
-	pokeName = str(raw_input("Enter the name of your Pokemon: ").lower())
-	numEvolv = pokedex[pokeName]
-except:
+def main(argv):
+    pokeName = "False"
+    numPoke = False
+    numEvolv = False
+    numCandy = False
+    numPokeNew = False
+    numCandyNew = False
+    numEvolvcount = False
+    with open('pokedex.json', 'r') as f:
+        pokedex = json.load(f)
+    parser = argparse.ArgumentParser(description='calc.py -p <pokemon> -n <number of pokemon> -c <number of pokemon candy>')
+    parser.add_argument('-p',help='Input Pokemon name',required=False)
+    parser.add_argument('-n',help='Input number of pokemon',required=False)
+    parser.add_argument('-c',help='Input number of candy',required=False)
+    args = parser.parse_args()
+    # If you later architect the script to run on a loop overall, I'd suggest 
+    # packing the whole script into a loop that iterates on command line arguments
+    
+    try:
+	if pokedex[args.p]:
+	    pokeName = str(args.p).lower()
+	else:
+	    pokeName = str(raw_input("Enter the name of your Pokemon: ").lower())
+    except:
 	print "Either you've spelled your Poke's name wrong, or your Poke cannot be evolved."
 	exit(1)
-# See above note about loops. In this case, you're trying to use a loop to 
-# replace a conditional, though the conditional doesn't need to be there 
-# anyway. What you really want is a try-except clause to take corrective
-# action if the input doesn't match what is expected.
-try:
-	numPoke = int(input("How many " + pokeName + " do you have? "))
-	numPoke >= 0
-except:
+    numEvolv = pokedex[pokeName]
+    try:
+	if args.n:
+	    numPoke = int(args.n)
+	else:
+	    numPoke = int(input("How many " + pokeName + " do you have? "))
+	numPoke >=0
+    except:
 	print "Please enter a number"
 	exit(2)
-try:
-	numCandy = int(input("How many " + pokeName + " candy do you have? "))
+    try:
+	if args.c:
+	    numCandy = int(args.c)
+	else:
+	    numCandy = int(input("How many " + pokeName + " candy do you have? "))
 	numCandy >= 0
-except:
+    except:
 	print "Please enter a number"
 	exit(3)
-# I don't see anything that really needs improvement past here, except for the
-# best practices mentioned above regarding variable declaration. 
-if (numCandy % numEvolv) != 0:
+    if (numCandy % numEvolv) != 0:
 	while ((numCandy + numCandyNew) % numEvolv) != 0:
+	    numPokeNew += 1
+	    numCandyNew += 3
+	    if numPokeNew >= 10:
+		break
+	if numPokeNew < 10:
+	    print "You can currently evolve %d %s" % ((numCandy / numEvolv), pokeName)
+	    print "You should hunt %d more %s to evolve %d" % (numPokeNew, pokeName, (numCandy / numEvolv) + 1)
+	else:
+	    numPokeNew = 0
+	    numCandyNew = 0
+	    while ((numCandy % numEvolv) + numCandyNew) < numEvolv:
 		numPokeNew += 1
 		numCandyNew += 3
-		if numPokeNew >= 10:
-			break
-	if numPokeNew < 10:
-		print "You can currently evolve %d %s" % ((numCandy / numEvolv), pokeName)
-		print "You should hunt %d more %s to evolve %d" % (numPokeNew, pokeName, (numCandy / numEvolv) + 1)
-	else:
-		numPokeNew = 0
-		numCandyNew = 0
-		while ((numCandy % numEvolv) + numCandyNew) < numEvolv:
-			numPokeNew += 1
-			numCandyNew += 3
-		print "You can currently evolve %d %s" % ((numCandy / numEvolv), pokeName)
-		print "You will need to hunt %d more %s to evolve %d" % (numPokeNew, pokeName, (numCandy / numEvolv) + 1)
-elif numCandy == 0:
+	    print "You can currently evolve %d %s" % ((numCandy / numEvolv), pokeName)
+	    print "You will need to hunt %d more %s to evolve %d" % (numPokeNew, pokeName, (numCandy / numEvolv) + 1)
+    elif numCandy == 0:
 	numPokeNew = 0
 	numCandyNew = 0
         while ((numCandy % numEvolv) + numCandyNew) < numEvolv:
-                numPokeNew += 1
-                numCandyNew += 3
-        print "You should hunt %d more %s to evolve %d" % (numPokeNew, pokeName, (numCandy / numEvolv) + 1)
-else:
+	     numPokeNew += 1
+             numCandyNew += 3
+	print "You should hunt %d more %s to evolve %d" % (numPokeNew, pokeName, (numCandy / numEvolv) + 1)
+    else:
 	print "You do not need any more %s to use all of your candy" % pokeName
+
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
